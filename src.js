51907f5aafefddcacbde8436bd7f487c09aee537
Isaac Dozier <isaactdozier@gmail.com>
2016-03-29 14:48:24 -0700

    Update src.js

--- src.js
+++ src.js
@@ -1,24 +1,45 @@
 function capFullName(full_name){
-  var clean_name = full_name.toLowerCase();
+  var clean_name_arr = full_name.toLowerCase().split(" ");
   var new_name;
-  var other = ["'"];
+  var surName = "mc";
+  var apo = "'";
   
-  var new_arr = clean_name.split(" ").map(function(a){
-    return a.replace(other, other+" ");
-  }).join(" ").split(" ");
+  console.log("--debug--");
+  console.log(new Date().getTime());
   
-  var new_caps = new_arr.map(function(subName){
-    var nameCaps;
+  var new_arr = clean_name_arr.map(function(a){
+    var tmp;
+    var frontName;
+    var backName;
     
-    nameCaps = subName.charAt(0).toUpperCase();
-    nameCaps = nameCaps.concat(subName.slice(1,subName.length));
-    
-    return nameCaps;
-    
-  });
+    if(a.startsWith(surName)){
+      backName = a.slice(surName.length, a.length);
+      tmp = a.replace(surName, oneWordUpper(surName));
+      tmp = tmp.replace(backName, oneWordUpper(backName));
+    } else if(a.includes(apo)){
+      frontName = a.slice(0, a.search(apo));
+      backName = a.slice(a.search(apo)+1, a.length);
+      tmp = a.replace(frontName, oneWordUpper(frontName));
+      tmp = tmp.replace(backName, oneWordUpper(backName));
+    } else {
+      tmp = oneWordUpper(a);
+    }
+    return tmp;
+  }); 
   
-  new_name = new_caps.join(" ").split("' ");
-  new_name = new_name.join("'");
+  new_name = new_arr.join(" ");
   
   return new_name;
+}
+
+//helper functions
+function isInArray(value, array) {
+  return array.indexOf(value) > -1;
+}
+
+function oneWordUpper(oneWord){
+    var Caps;
+    Caps = oneWord.charAt(0).toUpperCase();
+    Caps = Caps.concat(oneWord.slice(1,oneWord.length));
+    return Caps;  
 }
