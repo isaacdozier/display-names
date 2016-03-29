19a35d287ea9051ad82d82f193352a892376c1f8
Isaac Dozier <isaactdozier@gmail.com>
2016-03-29 16:04:48 -0700

    surname functions
    added functionality to allow an array of like surnames (ie. 'mc, mac') to be listed and treated alike.

--- src.js
+++ src.js
@@ -1,25 +1,64 @@
+//helper functions
+function isInArray(value, array) {
+  return array.indexOf(value) > -1;
+}
+
+function oneWordUpper(oneWord){
+  var Caps;
+  Caps = oneWord.charAt(0).toUpperCase();
+  Caps = Caps.concat(oneWord.slice(1,oneWord.length));
+  return Caps;  
+}
+
+function arrayStartsValue(array, value){
+  var tmp = false;
+  for(var i=0; i < array.length;i++){
+    if(value.startsWith(array[i])){
+      tmp = true;
+    }
+  }
+  return tmp;
+}
+
+//Builder function
 function capFullName(full_name){
   var clean_name_arr = full_name.toLowerCase().split(" ");
   var new_name;
-  var surName = "mc";
+  var surNameM = ["mc","mac"];
+  var surNameY = "y";
   var apo = "'";
-  
   var new_arr = clean_name_arr.map(function(a){
+    
     var tmp;
     var frontName;
     var backName;
     
-    if(a.startsWith(surName)){
-      backName = a.slice(surName.length, a.length);
-      tmp = a.replace(surName, oneWordUpper(surName));
+    if(arrayStartsValue(surNameM,a)){
+      
+      for(var i = 0;i < surNameM.length;i++){
+        if(a.startsWith(surNameM[i])){
+          backName = a.slice(surNameM[i].length, a.length);
+          tmp = a.replace(surNameM[i], oneWordUpper(surNameM[i]));
+        } 
+      }
+      
       tmp = tmp.replace(backName, oneWordUpper(backName));
+      
     } else if(a.includes(apo)){
+      
       frontName = a.slice(0, a.search(apo));
       backName = a.slice(a.search(apo)+1, a.length);
       tmp = a.replace(frontName, oneWordUpper(frontName));
       tmp = tmp.replace(backName, oneWordUpper(backName));
+      
+    } else if(a === surNameY){
+      
+      tmp = a;
+      
     } else {
+      
       tmp = oneWordUpper(a);
+      
     }
     return tmp;
   }); 
@@ -27,16 +66,4 @@ function capFullName(full_name){
   new_name = new_arr.join(" ");
   
   return new_name;
-}
-
-//helper functions
-function isInArray(value, array) {
-  return array.indexOf(value) > -1;
-}
-
-function oneWordUpper(oneWord){
-    var Caps;
-    Caps = oneWord.charAt(0).toUpperCase();
-    Caps = Caps.concat(oneWord.slice(1,oneWord.length));
-    return Caps;  
 }
